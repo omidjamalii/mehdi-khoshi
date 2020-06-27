36d1879816aefbf6e5382745b0c425bc3692da4c
Omid <o.jamali@hotmail.com>
2020-06-27 20:42:52 +0430

    mehdi p1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit , ViewChild , ElementRef, OnDestroy , ChangeDetectorRef} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit{
+  
+  ngOnInit(){
+    
+  } 
+  var video = document.getElementById("myVideo");
+  var btn = document.getElementById("myBtn");
+  
+  function myFunction() {
+    if (video.paused) {
+      video.play();
+      btn.innerHTML = "Pause";
+    } else {
+      video.pause();
+      btn.innerHTML = "Play";
+    }
+  }
+
+
 }
+
+
